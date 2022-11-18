a1384a6479d1924df30e849dad552b1b09748cd8
FawziLinggo <fawzilinggo@gmail.com>
2022-11-18 09:20:19 +0700

    add base repo producer

---
+++ spring-producer-activeMQ/src/main/java/developer/alldataint/com/springproduceractiveMQ/SpringProducerActiveMqApplication.java
@@ -0,0 +1,13 @@
+package developer.alldataint.com.springproduceractiveMQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProducerActiveMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProducerActiveMqApplication.class, args);
+	}
+
+}
---
+++ spring-producer-activeMQ/src/test/java/developer/alldataint/com/springproduceractiveMQ/SpringProducerActiveMqApplicationTests.java
@@ -0,0 +1,13 @@
+package developer.alldataint.com.springproduceractiveMQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProducerActiveMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
